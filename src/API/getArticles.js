249ff86524c96125164e9c8905b89789ee9ffa26
janismozgis-rcs <janismozgis+rcs@gmail.com>
2022-02-24 20:14:55 +0200

    lesson

---
+++ src/API/getArticles.js
@@ -0,0 +1,19 @@
+function getArticles() {
+
+    return [
+        {
+            title: 'Cat 1',
+            description: 'Just some article about an amazing cat 1',
+        },
+        {
+            title: 'Cat 2',
+            description: 'Just some article about an amazing cat 2',
+        },
+        {
+            title: 'Cat 3',
+            description: 'Just some article about an amazing cat 3',
+        },
+    ]
+}
+
+export default getArticles;
